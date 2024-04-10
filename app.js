19a9ada709c94608498701dc60354c0cf01f20c0
AlvaroPM <alvaropavonmartinez7@gmail.com>
2024-04-10 22:07:40 +0200

    añadido movimiento de los invasores, game over  y has ganado

--- app.js
+++ app.js
@@ -2,17 +2,30 @@
 const caja= document.querySelector(".caja");
 
 /*Seleccionamos el numero que mostrara el resultado */
-const resultado= document.querySelector(".resultado");
+const resultadoPntalla= document.querySelector(".resultado");
 
 /*altura por la que multiplicamos para generar los divs que haran los cuadrados dentro de la caja */
 const width= 15;
 
 /*array de invasores eliminados */
 const invasoresEliminados=[];
+
+/*indice de la nave */
 let indiceNaveActual=202;
+
+/*indice de los invasores */
+let invasoresIndice;
+
+/*variable movimiento a la derecha*/
+let movDerecho=true;
+
+
+let direccion=1;
+
 /* Creamos un for para añadir divs dentro de la caja donde se moveran*/
 for(let i=0; i<width*width; i++){
     const cuadrado= document.createElement("div");
+    cuadrado.id=i;
     caja.appendChild(cuadrado);
 }
 
@@ -44,18 +57,80 @@ dibujar();
 /*Añadir incio de la nave*/
 cuadrados[indiceNaveActual].classList.add("nave");
 
-/*Funcion para mover la nave*/
-function moverNave(){
-cuadrados[indiceNaveActual].classList.remove("nave");
-switch(e.key){
-    case"ArrowLeft":
-        if(indiceNaveActual % width !== 0){
-            indiceNaveActual -=1
-            break;
-        }
-        cuadrados[indiceNaveActual].classList.add("nave");
+/*Funcion para eliminar el id invasores de los cuadrados */
+function eliminar(){
+    for(let i=0; i<aliens.length;i++){
+        cuadrados[aliens[i]].classList.remove("invasor");
     }
 }
 
+/*Funcoin de movimiento de los invasores */
+function moverInvasores(){
+    const bordeIzquierdo= aliens[0] % width === 0;
+    const bordeDerecho= aliens[aliens.length -1] % width === width -1;
+    eliminar();
+
+    
+   if(bordeDerecho && movDerecho){
+        for(let i=0;i<aliens.length;i++){
+        aliens[i] += width +1;
+        direccion = -1;
+        movDerecho=false;
+        }
+    }
+
+    if(bordeIzquierdo &&!movDerecho){
+        for(let i=0; i<aliens.length; i++){
+            aliens[i] += width -1;
+            direccion=1,
+            movDerecho=true;
+        }
+    }
+    for(let i=0;i<aliens.length;i++){
+        aliens[i] += direccion;
+        }
+
+    dibujar();
+
+    /*Si dentro del mismo cuadro esta nave e invasor, resultado pasa a poner game over y se detiene el movimiento */
+    if(cuadrados[indiceNaveActual].classList.contains("invasor")){
+        resultadoPntalla.innerHTML = "GAME OVER";
+        clearInterval(invasoresIndice);
+    }
+    /*Si todos los invasores son eliminados, se genera el has ganasdo */
+    if(invasoresEliminados.length=== aliens.length){
+        resultadoPntalla="HAS GANADO";
+        clearInterval(moverInvasores);
+    }
+}
+
+/*Intervalo de movimiento de los invasores */
+invasoresIndice= setInterval(moverInvasores, 500);
+
+/*minuto 38.14, añadir disparo */
+
+/*Funcion para mover la nave*/
+function moverNave(e){
+    /*Eliminamos la nave del valor actual */
+cuadrados[indiceNaveActual].classList.remove("nave");
+switch(e.key){
+    case "ArrowLeft" :
+        if(indiceNaveActual % width !== 0){
+            indiceNaveActual -=1;
+        }
+        break;
+    case "ArrowRight" :
+        if(indiceNaveActual % width < width -1){
+            indiceNaveActual +=1;
+        }
+        break;
+    }
+    /*Añadimos la nave al nuevo valor */
+    cuadrados[indiceNaveActual].classList.add("nave");
+}
+
 document.addEventListener("keydown", moverNave);
-/*Minuto 25.50 video:  https://www.youtube.com/watch?v=s6LrpUTQQn0*/
+
+
+
+
